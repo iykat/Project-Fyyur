478b9257a735348eadd94a8b55135991ef301d8f
Prince Takyi <takyip28@gmail.com>
2022-05-31 10:19:36 +0000

    final commit

--- app.py
+++ app.py
@@ -38,10 +38,10 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
@@ -49,7 +49,7 @@ class Venue(db.Model):
     shows = db.relationship('Show', backref='Venue', lazy=True, cascade='all, delete-orphan')
     seeking_talent = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String(800))
-    website = db.Column(db.String(120))
+    website = db.Column(db.String(120)) 
     UniqueConstraint('name', 'city', 'state', 'address', name='unique_name_city_state_address')
     
     @property
@@ -75,11 +75,11 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False, unique=True)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.String()))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(500))
@@ -106,16 +106,12 @@ class Artist(db.Model):
       return len(self.past_shows)
     
 class Show(db.Model):
-      __tablename__ = 'Show'
+  __tablename__ = 'Show'
 
-      id = db.Column(db.Integer, primary_key=True)
-      start_time = db.Column(db.DateTime, nullable=False)
-      artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-      venue_id = db.column(db.Integer, db.ForeignKey('Venue.id'))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+  id = db.Column(db.Integer, primary_key=True)
+  start_time = db.Column(db.DateTime, nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -159,7 +155,7 @@ def venues():
       "name": "Park Square Live Music & Coffee",
       "num_upcoming_shows": 1,
     }]
-  }, {
+  }, {  
     "city": "New York",
     "state": "NY",
     "venues": [{
